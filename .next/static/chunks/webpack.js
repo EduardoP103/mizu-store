09d41000abdbf337825f6a8d758164763417b225
EduardoP103 <eduardopastor103@gmail.com>
2024-08-23 18:58:53 -0500

    se actualiza el webpack

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff67e9eff4346ea7"; }
+/******/ 		__webpack_require__.h = function() { return "e10cc8d0139803a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
